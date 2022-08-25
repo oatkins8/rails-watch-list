affc18fd7d23dd84afc67c4682c854cd1cb6c704
Oliver Atkins <ollieatkins8@outlook.com>
2022-08-25 13:06:01 +0100

    completed validations and associations

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # resources :movies do
+  #   resources :bookmarks
+  # end
+
+  # resources :lists do
+  #   resources :bookmarks
+  # end
+
 end
