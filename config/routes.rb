c2b7f6292d4c65a1764206306ad74696e3f6ad00
Oliver Atkins <ollieatkins8@outlook.com>
2022-08-26 16:45:11 +0100

    heroku changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "lists#index"
+  root to: "lists#index"
 
   resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
